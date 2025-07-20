3c85608084a602848a353e54fda332289a6ce679
kayqueGovetri <govetriKayque@gmail.com>
2025-07-20 20:30:59 -0300

    feat: insert arguments in macos

--- entrypoint-macos.sh
+++ entrypoint-macos.sh
@@ -2,7 +2,10 @@
 set -e
 
 PORT="${INPUT_MYSQL_PORT:-32768}"
-PASSWORD="${INPUT_MYSQL_ROOT_PASSWORD:-root}"
+ROOT_PASSWORD="${INPUT_MYSQL_ROOT_PASSWORD:-root}"
+DATABASE="${INPUT_MYSQL_DATABASE:-}"
+USER="${INPUT_MYSQL_USER:-}"
+USER_PASSWORD="${INPUT_MYSQL_PASSWORD:-}"
 
 echo "### Installing MySQL on macOS"
 brew update
@@ -31,13 +34,28 @@ echo "### Waiting for MySQL to start"
 sleep 20
 
 echo "### Updating root password with plugin caching_sha2_password"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '${PASSWORD}'; FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '${ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
 
-echo "### Attempting MySQL connection"
-if ! mysql -h 127.0.0.1 -P $PORT -u root -p$PASSWORD -e "SELECT VERSION();"; then
-    echo "❌ Failed to connect to MySQL"
+if [ -n "$USER" ]; then
+  echo "### Creating user and database (if specified)"
+  mysql -u root -p"$ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$DATABASE\`;"
+  mysql -u root -p"$ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$USER_PASSWORD';"
+  mysql -u root -p"$ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`$DATABASE\`.* TO '$USER'@'%'; FLUSH PRIVILEGES;"
+fi
+
+echo "### Attempting MySQL root connection"
+if ! mysql -h 127.0.0.1 -P $PORT -u root -p"$ROOT_PASSWORD" -e "SELECT VERSION();"; then
+    echo "❌ Failed to connect to MySQL as root"
     tail -n 100 /opt/homebrew/var/mysql/error.log || echo "⚠️ Log not found"
     exit 1
+fi
+
+if [ -n "$USER" ]; then
+  echo "### Attempting MySQL custom user connection"
+  if ! mysql -h 127.0.0.1 -P $PORT -u "$USER" -p"$USER_PASSWORD" -e "SELECT CURRENT_USER();"; then
+    echo "❌ Failed to connect to MySQL as custom user"
+    exit 1
+  fi
 fi
 
 echo "✅ MySQL setup and connection successful"
